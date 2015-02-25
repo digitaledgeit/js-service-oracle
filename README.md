06517da29ad6b121dcf7b1c24d9611bc0ef31306
James Newell <james@digitaledgeit.com.au>
2015-02-26 02:40:10 +1100

    added doco

--- README.md
+++ README.md
@@ -9,6 +9,45 @@ The all-knowing Oracle knows *all* about your services!
 <small>Crap name I know, `service-locator` was taken but you can suggest something better.</small>
 
 
+## Installation
+
+    npm install --save service-oracle
+    
+## Usage
+    
+    var locator = require('service-oracle');
+    
+    //setup your services
+    var services = locator()
+    	.set('Config', require('./config.json'))
+    	.factory('Database', function(locator) {
+    		var config    = locator.get('Config');
+    		var database  = new Database(config.db);
+    		return database;
+    	})
+    	.factory('Users', function(locator) {
+    		var database  = locator.get('Database');
+    		var users     = new Users(database);
+    		return users;
+    	})
+    	.factory('Posts', function(locator) {
+    		var database  = locator.get('Database');
+    		var posts     = new Posts(database);
+    		return posts;
+    	})
+    	.factory('BlogService', function(locator) {
+    		var users     = locator.get('Config');
+    		var posts     = locator.get('Config');
+    		var service   = new BlogService(users, posts);
+    		return service;
+    	})
+    ;
+    
+    //somewhere else e.g. in your express routes
+    var service = services.get('BlogService');
+    
+
+
 ## API
 
 ### Methods
--- example/index.js
+++ example/index.js
@@ -1,24 +1,31 @@
-var ServiceLocator = require('..');
+var locator = require('..');
 
-var locator = new ServiceLocator();
-locator
-	.set('Eyes', [{type: 'Eye'}, {type: 'Eye'}])
-	.set('Ears', [{type: 'Ear'}, {type: 'Ear'}])
-	.set('Nose', {type: 'Nose'})
-	.set('Mouth', {type: 'Mouth'})
-	.factory('Hair', './example/hair')
-	.factory('Face', function(locator) {
-		return {
-			eyes: locator.get('Eyes'),
-			ears: locator.get('Ears'),
-			nose: locator.get('Nose'),
-			mouth: locator.get('Mouth'),
-			hair: locator.get('Hair')
-		}
+//setup your services
+var services = locator()
+	.set('Config', require('./config.json'))
+	.factory('Database', function(locator) {
+		var config    = locator.get('Config');
+		var database  = new Database(config.db);
+		return database;
+	})
+	.factory('Users', function(locator) {
+		var database  = locator.get('Database');
+		var users     = new Users(database);
+		return users;
+	})
+	.factory('Posts', function(locator) {
+		var database  = locator.get('Database');
+		var posts     = new Posts(database);
+		return posts;
+	})
+	.factory('BlogService', function(locator) {
+		var users     = locator.get('Config');
+		var posts     = locator.get('Config');
+		var service   = new BlogService(users, posts);
+		return service;
 	})
 ;
 
-var face1 = locator.get('Face');
-var face2 = locator.get('Face');
-var face3 = locator.new('Face');
-console.log(face1 === face2 !== face3);
+//somewhere else e.g. in your express routes
+var service = services.get('BlogService');
+
